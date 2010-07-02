1f671539019a24325524486221dca527d53a5a36
Jonathan E. Magen <yonkeltron@gmail.com>
2010-07-01 20:30:03 -0400

    added exception handling to twitter plugin

--- libscosugbot/bot.rb
+++ libscosugbot/bot.rb
@@ -112,8 +112,14 @@ module LibScosugBot
         nick = m.args[:nick]
         m.reply "Fetching last tweet for #{nick}..."
         db.log(2, "Fetching tweets for #{nick}", 'twitter')
-        LibScosugBot::Utils::Twitter.get_last_twitter_statuses(nick).each do |tweet|
-          m.reply "#{nick} tweeted \"#{tweet['text']}\" at #{tweet['created_at']}"
+        begin
+          LibScosugBot::Utils::Twitter.get_last_twitter_statuses(nick).each do |tweet|
+            m.reply "#{nick} tweeted \"#{tweet['text']}\" at #{tweet['created_at']}"
+          end
+        rescue Exception => e
+          message = "Error fetching tweets: #{e}"
+          db.log(5, message)
+          m.reply message
         end
       end
     end    
